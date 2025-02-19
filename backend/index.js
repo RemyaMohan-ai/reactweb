a40674feb44cc81ba7c3e51833b991bf112e9a66
remyamohan <remya04mohan@gmail.com>
2025-02-19 16:24:04 +0530

    user model created

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() =>{
+    console.log("connectedd to db");   
+}).catch((err)=>{
+    console.log("error in db",err);
+})
 
 const app = express()
 
