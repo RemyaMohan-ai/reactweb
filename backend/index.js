c599e100552ea46f63e7a596f7f50f6d42c19769
remyamohan <remya04mohan@gmail.com>
2025-02-21 00:28:16 +0530

    signup functionality completed

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
-import userRoute from '../routes/userRoute.js'
-import authRoute from '../routes/authRoute.js'
+import userRoute from './routes/userRoute.js'
+import authRoute from './routes/authRoute.js'
 
 mongoose.connect(process.env.MONGO)
 .then(() =>{
@@ -16,14 +16,14 @@ mongoose.connect(process.env.MONGO)
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extends:true}))
+// app.use(express.urlencoded({extends:true}))
 
 app.listen(3000, () => {
     console.log('server listening on port 300');
     
 })
-app.use('/',userRoute)
-app.use('/auth',authRoute)
+app.use('/api/user',userRoute)
+app.use('/api/auth',authRoute)
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || "internal server error"
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 const Signup = () => {
+   const[formdata ,setFormdata] = useState({})
+   const[loading ,setLoading] = useState(false)
+   const[error ,setError] = useState(null)
+  const handleChange = (e)=>{
+    setFormdata({...formdata,[e.target.id]:e.target.value})
+  }
+  console.log(formdata);
+  
+
+ const handleSubmit =async(e)=>{
+   e.preventDefault()
+   try {
+    setLoading(true);
+    setError(false)
+    const res = await fetch('/api/auth/signup',{
+     method:"POST",
+     headers :{
+       "Content-Type":'APPlication/json',
+     },
+     body : JSON.stringify(formdata)
+    });
+    const data = await res.json()
+    console.log(data);
+    setLoading(false)
+    if(data.success == false){
+      setError(true)
+      return;
+    }
+ 
+   }
+    catch (error) {
+      setLoading(false)
+      setError(true)
+    
+   }
+
+ }
+
   return (
     <div className='p-4 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7 '>signup</h1>
       <div>
-        <form action="" className='flex flex-col gap-5'>
-          <input type="text" name="" id="userName" placeholder='username' className='bg-slate-100 rounded-md p-3'/>
-          <input type="email" name="" id="email" placeholder='email'className='bg-slate-100 rounded-md p-3'/>
-          <input type="password" name="" id="password" placeholder='password' className='bg-slate-100 rounded-md p-3'/>
-          <button className='uppercase bg-purple-950 text-white rounded-lg p-3 hover:opacity-90 cursor-pointer disabled:opacity-70 '>signup</button>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+          <input type="text" name="" id="userName" placeholder='username' className='bg-slate-100 rounded-md p-3' 
+          onChange={handleChange}/>
+          <input type="email" name="" id="email" placeholder='email'className='bg-slate-100 rounded-md p-3' 
+          onChange={handleChange}/>
+          <input type="password" name="" id="password" placeholder='password' className='bg-slate-100 rounded-md p-3' 
+          onChange={handleChange}/>
+          <button disabled={loading} className='uppercase bg-purple-950 text-white rounded-lg p-3 hover:opacity-90 cursor-pointer disabled:opacity-70 '> {loading ? 'Loading...' : 'Sign Up'}</button>
         </form>
       </div>
       <div className='flex pt-4'>
@@ -19,6 +60,7 @@ const Signup = () => {
 
         </Link>
       </div>
+      <p>{setError && "something went wrong"}</p>
     </div>
   )
 }
